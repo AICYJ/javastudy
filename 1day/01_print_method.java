3fce6609400f31638eb62b19dcdaf73391bd9d25
CYJ <gkdlfnddy@gmail.com>
2021-11-15 13:17:22 +0900

    day 01 update

---
+++ 1day/01_print_method.java
@@ -0,0 +1,33 @@
+package day01;
+
+public class 01_print_method {
+
+	public static void main(String[] args) {
+//		 TODO Auto-generated method stub
+		System.out.print("sentence !!");//sentence input
+		System.out.print('A');//character
+		System.out.print(100);//integer
+		System.out.print(100.124);//real number
+		System.out.println();//input with enter
+		
+//		println() works with enter show below
+		System.out.println("sentence !!");
+		System.out.println('A');
+		System.out.println(100);
+		System.out.println(100.124);
+//		printf() works with type of value and value
+//		show below example
+		System.out.println();
+		System.out.printf("%s","sentence !!");//string s
+		System.out.println();
+		System.out.printf("%c",'A');//charcater c
+		System.out.println();
+		System.out.printf("%c",65);//c refers ascii code
+		System.out.println();
+		System.out.printf("%d",100);//decimal d
+		System.out.println();
+		System.out.printf("%f",100.124);//float f
+	
+	}
+
+}
