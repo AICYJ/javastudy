051683785d00630bdb2ea8a9d1202bc4e9bb8f88
CYJ <gkdlfnddy@gmail.com>
2021-12-22 19:55:20 +0900

    99steps for upload

---
+++ 04_00_Control_flow_statement/A_03_for_99step.java
@@ -0,0 +1,11 @@
+public class A_03_for_99step {
+    public static void main(String[] args) {
+        System.out.println("print 99 steps!!");
+        for (int step1=2;step1<10;step1++) {
+            for (int step2=2;step2<10;step2++) {
+                System.out.println(step1+"X"+step2+"="+(step1*step2));
+            }
+        }
+    }
+
+}
